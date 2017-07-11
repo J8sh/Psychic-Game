e4317615d8daf2bc6a4b8d01d992e28749cc54b7
Josh <j8shigarcia@aol.com>
2017-07-11 17:56:17 -0500

    update!

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+
+
+// Array of alphabet letters for computer to pick from
+
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Set variables
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+// guessesSoFar is an array that will hold all the user's guesses
+var guessesSoFar = [];
+// userGuess is what the user picks by pressing a key
+var userGuess = null;
+// Have computer pick a letter and store it in letterToBeGuessed
+var letterToBeGuessed = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+
+
+document.onkeyup = function(event) {
+
+  // When user presses a key and shows on user quess list
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  // Adds user's guess to guessesSoFar array
+  if (guessesSoFar.indexOf(userGuess) < 0 && computerChoice.indexOf(userGuess) >= 0) {
+    guessesSoFar[guessesSoFar.length]=userGuess;
+    
+    guessesLeft--;
+  }
+
+  // records loss and resets game
+  if (guessesLeft == 0) {
+    losses++;
+    console.log("You lost!");
+    guessesLeft = 9;
+    guessesSoFar = [];
+    letterToBeGuessed = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+  }
+
+  // records win and resets game
+  if (letterToBeGuessed == userGuess) {
+    wins++;
+    console.log("You won!");
+    guessesLeft = 9;
+    guessesSoFar = [];
+    letterToBeGuessed = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+  }
+
+  // Displays HTML
+  var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I\'m thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+  
+  document.querySelector("#game").innerHTML = html;
+
+}
